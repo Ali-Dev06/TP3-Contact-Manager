a4ae74f91689f4a442c8b5f624bafcb2d7a7c785
Ali <your.email@example.com>
2026-05-17 19:04:50 +0100

    Completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from './contact';
+import { Contact } from './contact.interface';
+import { ListeContactsComponent }     from './liste-contacts/liste-contacts';
+import { FormulaireContactComponent } from './formulaire-contact/formulaire-contact';
+import { StatsContactsComponent }     from './stats-contacts/stats-contacts';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    ListeContactsComponent,
+    FormulaireContactComponent,
+    StatsContactsComponent
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('TP3-Contact-Manager');
+export class AppComponent implements OnInit {
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    console.log('App initialisée. Contacts :', this.contactService.getAll().length);
+  }
+
+  onContactAjoute(contact: Contact): void {
+    this.contactService.ajouter(contact);
+  }
 }
